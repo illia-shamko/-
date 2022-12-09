01a79c95bb3e1bae4029a57f94b25a2b809949d6
illia-shamko <120163800+illia-shamko@users.noreply.github.com>
2022-12-09 12:07:04 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,13 @@
+w1 = input("Введіть перше слово: ")
+w2 = input("Введіть друге слово: ")
+
+try:
+    file = open("text.txt",encoding='utf8')
+    text = file.read()
+
+    result = text.replace(w1, w2)
+    print(result)
+except Exception as e:
+    print("Помилка: " + str(e))
+
+print("Ви ввели неправильні параметри.")
